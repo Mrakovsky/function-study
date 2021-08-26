11bc0d68603cfd140b68837a0a62afed298b1e09
Mrakovsky <soutam716@gmail.com>
2021-08-27 00:38:21 +0900

    26日夜作成練習問題コメントアウト

--- function.js
+++ function.js
@@ -1,1 +1,8 @@
-console.log("試験")
+let birthTime = new Date(2001,10,07,12,51);
+function p(params) {
+    let now = new Date();
+    let seconds = (now.getTime() - birthTime.getTime()) / 1000;
+    document.getElementById('birth-time').innerText =
+      '生まれてから' + seconds + '秒経過。';
+  }
+setInterval(p,90);
